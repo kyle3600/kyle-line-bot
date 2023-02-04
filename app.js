81862775c25e15e4f14b30de8a23f4f88055e705
kyle.chang <kyle3600@gmail.com>
2023-02-04 18:29:42 +0800

    change text-davinci-003

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ async function handleEvent(event) {
   }
 
   const completion = await openai.createCompletion({
-    model: "code-davinci-003",
+    model: "text-davinci-003",
     prompt: event.message.text ,
     max_tokens: 500,
   });
